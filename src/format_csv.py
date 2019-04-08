3768c972117dce0fdd67a389ccd1c24ec2580b9c
Csanaki Richárd <richard.csanaki@gmail.com>
2019-04-08 12:23:56 +0200

    update

--- src/format_csv.py
+++ src/format_csv.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
-with open('download2.csv', 'r') as f_in, open('formatted_download2.csv', 'w') as f_out:
+with open('download.csv', 'r') as f_in, open('formatted_download.csv', 'w') as f_out:
     f_out.write(next(f_in))
     [f_out.write(','.join(line.split()) + '\n') for line in f_in]
+
+
+
+
+
